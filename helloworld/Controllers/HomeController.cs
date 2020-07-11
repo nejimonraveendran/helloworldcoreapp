03a1dfff456769f0682e2ed0f6dc676db91683b9
Nejimon Raveendran <Nejimon.Raveendran@teranet.ca>
2020-07-11 00:45:10 -0400

    version 4

--- helloworld/Controllers/HomeController.cs
+++ helloworld/Controllers/HomeController.cs
@@ -5,9 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using helloworld.Models;
+using System.IO;
 
 namespace helloworld.Controllers
 {
+    public class Input
+    {
+        public string Text { get; set; }
+    }
+
     public class HomeController : Controller
     {
         public IActionResult Index()
@@ -25,5 +31,32 @@ namespace helloworld.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost("/api/file")]
+        public string CreateFile(Input input)
+        {
+            var request = Request.Body;
+
+            var fileName = Guid.NewGuid().ToString() + ".txt";
+            using (var writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine(input.Text);
+            }
+
+            return fileName;
+        }
+
+
+        [HttpGet("/api/file/{id}")]
+        public string ReadFile(string id)
+        {
+            using (var reader = new StreamReader(id))
+            {
+                return reader.ReadToEnd();
+            }
+
+        }
+
+
     }
 }
